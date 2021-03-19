06230e9490a9c8b8df028dabcfbc48410bd344d0
Frenzoid <frenzoid@protonmail.com>
2021-03-19 09:11:42 -0500

    Automatic commit. Commit date: Fri 19 Mar 2021 09:11:42 AM CDT

--- automated/compressallbz2.sh
+++ automated/compressallbz2.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
-
 # For each path inside the first argument passed to the script.
 for i in $(find $1); do
-
 # Check f path is a file, and theres no compressed version of the file, and file is not the compressed version.
  if [[ -f $i && ! -f $i.bz2 && $i != *bz2 && $i != $0 ]]; then
    echo "[bzip2]: compressing $i...";
@@ -10,5 +8,4 @@ for i in $(find $1); do
    echo "[bzip2]: $i compressed.";
    echo "";
  fi
-
 done
